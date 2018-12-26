70c1ee9b3d1ea9475cd450c8b2ea4971910e4901
Daniel Molenda <danielmolenda16@gmail.com>
2018-12-26 11:51:14 +0100

    add function

--- generatorPassword.php
+++ generatorPassword.php
@@ -1,11 +1,16 @@
 <?php
 
-$length = readline("Jakiej dlugosci haslo wygenerowac? ");
-$pass = "";
+    function generatorPass()
+    {
+        $length = readline("Jakiej dlugosci haslo wygenerowac? ");
+        $pass = "";
 
-for ($i = 0; $i < $length; $i++)
-{
-    $pass .= chr(rand(32, 126));
-}
+        for ($i = 0; $i < $length; $i++)
+        {
+            $pass .= chr(rand(32, 126));
+        }
+        echo $pass;
+    }
 
-echo $pass;
+    generatorPass();
+
